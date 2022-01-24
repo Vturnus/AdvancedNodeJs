373551fe252e3f9f9a3fcfbb057e0faa2bc18f5e
William Anderson <amir.survivor@gmail.com>
2022-01-24 14:27:42 +0330

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,9 @@
 module.exports = {
   googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://localhost:27017/blog_dev',
-  cookieKey: 'MyFriendHopeIsAPrison@V21',
+    id,
+  googleClientSecret: secret,
+  mongoURI: URI,
+  cookieKey: Key,
   redisUrl: '127.0.0.1',
   redisPort: 6379
 };
